fa57cb7a9d2d0f5e2cce8e5f28902625bbc3133a
henk <hkripper@gmail.com>
2020-11-28 23:51:10 +0100

    http requests

--- DoYourDishes/DoYourDishes/src/main/java/com/control/logic/AsynchronousRequest.java
+++ DoYourDishes/DoYourDishes/src/main/java/com/control/logic/AsynchronousRequest.java
@@ -1,20 +1,25 @@
 package com.control.logic;
 
 import android.os.AsyncTask;
+import android.widget.TextView;
 
+import com.view.R;
 import com.view.gui.MainActivity;
 
 public class AsynchronousRequest extends AsyncTask<Void, Void, Void> {
 
     private MainActivity mainActivity;
 
-    private String data;
+    private TextView textView;
+
+    private String newText;
 
     private Crud crud = new Crud();
 
-    public AsynchronousRequest(MainActivity a,String passedString ) {
+    public AsynchronousRequest(MainActivity a, TextView passedTextView) {
         this.mainActivity = a;
-        data = passedString;
+        textView = passedTextView;
+        newText = "null";
     }
 
     @Override
@@ -29,7 +34,7 @@ public class AsynchronousRequest extends AsyncTask<Void, Void, Void> {
         // Here you are in the worker thread and you are not allowed to access UI thread from here
         //Here you can perform network operations or any heavy operations you want.
         try {
-            data = crud.fetchData();
+            newText = crud.fetchData();
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -42,7 +47,8 @@ public class AsynchronousRequest extends AsyncTask<Void, Void, Void> {
         //After completing execution of given task , control will return here.
         //Hence if you want to populate UI elements with fetched data, do it here
 
-        mainActivity.changeTextFromAsync(data);
+        //mainActivity.changeTextFromAsync(data);
+        textView.setText(newText);
     }
 
     @Override
