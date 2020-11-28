49b02d294b79389355210da4614d42a34c9e85ba
hdi10 <s0551006@htw-berlin.de>
2020-11-28 23:48:44 +0100

    bluetooth prototyp

---
+++ DoYourDishes/DoYourDishes/src/main/java/com/view/gui/ASAPRootActivity.java
@@ -0,0 +1,11 @@
+package com.view.gui;
+
+import net.sharksystem.asap.android.apps.ASAPActivity;
+import net.sharksystem.asap.android.apps.ASAPApplication;
+
+public class ASAPRootActivity extends ASAPActivity {
+
+    public ASAPRootActivity(ASAPApplication asapApplication) {
+        super(asapApplication);
+    }
+}
